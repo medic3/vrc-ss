c9766a1ab06008a33f207e556a17dada8867f41e
medic3 <happy450111@yahoo.co.jp>
2020-09-19 01:33:47 +0900

    de

--- #init.bat
+++
@@ -1,4 +0,0 @@
-git init
-git add .
-git commit -m "Initial Commit"
-git remote add origin https://github.com/medic3/vrc-ss.git
--- #push.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -am "New Commit"
-git push -fu origin master
